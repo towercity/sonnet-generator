3cac8a9ff76e35238d8dc1a002be224cfc37f160
towercity <matthewjnerger3@gmail.com>
2016-10-04 17:14:25 -0400

    Init repository from sonnetmakin_ebooks

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sonnet Generator
+
+A simple python-based web-app that generates a new Shakespearian sonnet every time you reload the page. The randomization
+algorythm is based off the Markov chain, and is modified from tommeagher's [Heroku_ebooks](https://github.com/tommeagher/heroku_ebooks)
---
+++ notes.py
@@ -0,0 +1,10 @@
+"""
+Store the entirity of the sonnets as a string. Add a function in ebooks.py
+that iterates thru it and splits it into an array by linebreaks ('\n'). Then use
+this array in the place of the tweets array from the original program. Yes!
+
+tweets.split('\n')
+
+Also, filter out lines that start with:
+XC LX II IV VI CX L. C. CL LI. LII
+"""
